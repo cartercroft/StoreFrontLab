b2e297ce570ea04500c25be333c81c04900ef0b2
Carter Croft <carterncroft@gmail.com>
2021-05-07 14:42:12 -0500

    Added Classes and Metadata for all DB Objects, created contact form and implemented email functionality

---
+++ StoreFrontLab.UI/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreFrontLab.UI.Models
+{
+    public class ContactViewModel
+    {
+        //Auto-prop syntax ("prop" tab tab) - generates fields at runtime
+        [Required(ErrorMessage = "*Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email is required.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message is required.")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
